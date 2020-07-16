01ffeff566a181edf1f4cf5bc9683a34be0ba3ee
Laksh <lakshmanansathya@gmail.com>
2020-07-16 01:35:00 -0400

    first commit

---
+++ celpip_pdf.py
@@ -0,0 +1,182 @@
+pages = ["https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-1-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-2-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-3-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-4-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-5-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-6-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-7-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-8-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-9-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-10-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-11-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-12-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-13-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-14-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-15-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-16-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-17-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-18-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-19-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-20-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-21-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-22-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-23-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-24-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-25-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-26-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-27-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-28-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-29-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-30-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-31-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-32-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-33-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-34-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-35-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-36-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-37-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-38-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-39-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-40-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-41-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-42-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-43-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-44-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-45-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-46-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-47-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-48-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-49-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-50-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-51-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-52-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-53-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-54-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-55-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-56-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-57-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-58-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-59-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-60-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-61-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-62-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-63-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-64-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-65-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-66-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-67-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-68-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-69-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-70-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-71-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-72-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-73-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-74-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-75-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-76-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-77-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-78-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-79-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-80-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-81-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-82-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-83-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-84-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-85-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-86-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-87-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-88-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-89-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-90-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-91-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-92-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-93-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-94-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-95-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-96-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-97-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-98-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-99-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-100-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-101-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-102-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-103-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-104-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-105-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-106-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-107-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-108-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-109-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-110-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-111-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-112-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-113-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-114-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-115-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-116-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-117-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-118-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-119-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-120-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-121-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-122-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-123-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-124-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-125-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-126-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-127-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-128-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-129-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-130-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-131-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-132-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-133-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-134-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-135-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-136-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-137-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-138-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-139-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-140-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-141-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-142-1024.jpg?cb=1542400126",
+"https://image.slidesharecdn.com/celpipstudyguide-181116202451/95/celpip-study-guide-143-1024.jpg?cb=1542400126"]
+
+
+# pipenv install fpdf
+from fpdf import FPDF
+pdf = FPDF()
+
+for i, page in enumerate(pages):
+    image = "images/" + page.split('/')[5].split("?")[0]
+    pdf.add_page()
+    pdf.image(image,x=0,y=0,w=210,h=297) # for A4 size because some people said that every other page is blank
+    print(i)
+pdf.output("merged.pdf", "F")
+
+# import os
+# import img2pdf
+
+# images = [i for i in os.listdir('images/') if i.endswith(".jpg")]
+# images.sort()
+# print(images[:10])
+# with open("celpip_generated.pdf", "wb") as f:
+#     f.write(img2pdf.convert([))
+
+# brew install graphicsmagick
+# brew install boost-python3
+# pipenv install pgmagick
+
+# import os
+# from os import listdir
+# from os.path import isfile, join 
+# from pgmagick import Image
+
+# mypath = "images/" # path to your Image directory 
+
+# for each_file in listdir(mypath):
+#     if isfile(join(mypath,each_file)):
+#         image_path = os.path.join(mypath,each_file)
+#         pdf_path =  os.path.join(mypath,each_file.rsplit('.', 1)[0]+'.pdf')
+#         img = Image(image_path)
+#         img.write(pdf_path)
